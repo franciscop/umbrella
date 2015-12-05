6ecefea0728207870837de3444c5fe377b799922
FranciscoP <franciscopresencia@hotmail.com>
2015-12-05 14:00:20 +0100

    Added style to the tests

--- test/test.js
+++ test/test.js
@@ -1,35 +1,32 @@
 var expect = chai.expect;
 
 // Testing the main file
-describe("Umbrella", function() {
+describe("Constructor", function() {
+  it("should be defined", function() {
+    expect(u).to.be.not.null;
+  });
+
+  it("should be a function", function() {
+    expect(typeof u).to.equal("function");
+  });
   
-  describe("constructor", function() {
-    it("should be defined", function() {
-      expect(u).to.be.not.null;
-    });
+  it("can accept no argument", function() {
+    expect(typeof u()).to.equal('object');
+  });
+  
+  it("can select by class", function() {
+    expect(u('.demo').nodes.length).to.equal(1);
+  });
+  
+  it("can select by tag", function() {
+    expect(u('body').nodes.length).to.equal(1);
+  });
+  
+  it("can select by id", function() {
+    expect(u('#demo').nodes.length).to.equal(1);
+  });
 
-    it("should be a function", function() {
-      expect(typeof u).to.equal("function");
-    });
-    
-    it("can accept no argument", function() {
-      expect(typeof u()).to.equal('object');
-    });
-    
-    it("can select by class", function() {
-      expect(u('.demo').nodes.length).to.equal(1);
-    });
-    
-    it("can select by tag", function() {
-      expect(u('body').nodes.length).to.equal(1);
-    });
-    
-    it("can select by id", function() {
-      expect(u('#demo').nodes.length).to.equal(1);
-    });
-
-    it("can select with css", function() {
-      expect(u('[id="demo"]').nodes.length).to.equal(1);
-    });
+  it("can select with css", function() {
+    expect(u('[id="demo"]').nodes.length).to.equal(1);
   });
 });
