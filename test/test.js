f7941cedfa39c69ea75e174d1aefebf514e3cc24
FranciscoP <franciscopresencia@hotmail.com>
2016-01-16 07:19:18 +0100

    Added testing

--- test/test.js
+++ test/test.js
@@ -332,62 +332,22 @@ describe(".first()", function() {
   });
 });
 // Testing the main file
-describe(".addClass(name1, name2, ...)", function() {
-  
-  beforeEach(function(){
-    expect(base.hasClass('bla')).to.equal(false);
-    expect(base.hasClass('blu')).to.equal(false);
-  });
-  
-  afterEach(function(){
-    base.removeClass('bla');
-    base.removeClass('blu');
-  });
+describe("parseJsom(string)", function() {
   
   it("should be defined", function() {
-    expect(typeof base.addClass).to.equal('function');
+    expect(typeof parseJson).to.equal('function');
   });
 
   it("can be called empty", function() {
-    base.addClass();
-    base.addClass("");
-    base.addClass([]);
-    base.addClass("","");
-    base.addClass(" ");
+    expect(parseJson()).to.equal(false);
+    expect(parseJson("")).to.equal(false);
+    expect(parseJson([])).to.equal(false);
+    expect(parseJson("","")).to.equal(false);
+    expect(parseJson(" ")).to.equal(false);
   });
 
-  it("adds a class", function() {
-    base.addClass('bla');
-    expect(base.hasClass('bla')).to.equal(true);
-  });
-
-  it("adds several classes as arguments", function() {
-    base.addClass('bla', 'blu');
-    expect(base.hasClass('bla')).to.equal(true);
-    expect(base.hasClass('blu')).to.equal(true);
-  });
-
-  it("adds several classes with an array", function() {
-    base.addClass(['bla', 'blu']);
-    expect(base.hasClass('bla')).to.equal(true);
-    expect(base.hasClass('blu')).to.equal(true);
-  });
-
-  it("adds several classes separated by space", function() {
-    base.addClass('bla blu');
-    expect(base.hasClass('bla')).to.equal(true);
-    expect(base.hasClass('blu')).to.equal(true);
-  });
-
-  it("adds several classes separated by comma", function() {
-    base.addClass('bla,blu');
-    expect(base.hasClass('bla')).to.equal(true);
-    expect(base.hasClass('blu')).to.equal(true);
-  });
-
-  it("adds several classes separated by comma", function() {
-    len = base.addClass('bla,blu').nodes.length;
-    expect(len).to.equal(1);
+  it("parses correctly", function() {
+    expect(parseJson('{"hello": "world"}').hello).to.equal('world');
   });
 });
 // Testing the main file
