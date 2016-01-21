0d6fa5ee96917f7d63c23da966b83234e81b61a5
FranciscoP <franciscopresencia@hotmail.com>
2016-01-21 19:23:54 +0100

    Added more performance tests

--- src/test.js
+++ src/test.js
@@ -18,33 +18,6 @@ describe("u(selector, context)", function() {
     expect(u('.demo').nodes.length).to.equal(1);
   });
   
-  it("select by class 100.000/second", function() {
-    
-    var init = new Date().getTime();
-    for (var i = 0; i < 10000; i++) {
-      u('.demo');
-    }
-    var uTime = new Date().getTime() - init;
-    
-    expect(uTime).to.be.below(100, uTime + ' ms');
-  });
-  
-  it("select by class is comparable to jquery (50% margin)", function() {
-    
-    var init = new Date().getTime();
-    for (var i = 0; i < 10000; i++) {
-      u('.demo');
-    }
-    var uTime = new Date().getTime() - init;
-    init = new Date().getTime();
-    for (var j = 0; j < 10000; j++) {
-      $('.demo');
-    }
-    var $Time = new Date().getTime() - init;
-    
-    expect(uTime).to.be.below($Time * 1.5, uTime + ' ms');
-  });
-  
   it("can select by tag", function() {
     expect(u('body').nodes.length).to.equal(1);
   });
@@ -67,8 +40,143 @@ describe("u(selector, context)", function() {
     var context = u('.demo li').nodes[0];
     expect(u('a', context).nodes.length).to.equal(1);
   });
+});
+
+
+
+
+
+describe("performance tests", function(){
+  
+  function performance(callback, times){
+    var init = new Date().getTime();
+    for (var i = 0; i < times; i++) {
+      callback(i);
+    }
+    return new Date().getTime() - init;
+  }
+  
+  // Generate a big and varied 100 element table
+  before(function(){
+    performance(function(i){
+      u('.performance').append('<tr class="ro"><td id="idn' + i + '"></td><td class="tabletest"></td><td></td><td></td></tr>');
+    }, 1000);
+  });
   
   
+  
+  it("simple select by class 100.000/second", function() {
+    
+    uTime = performance(function(){
+      u('.demo');
+    }, 10000);
+    
+    expect(uTime).to.be.below(100, uTime + ' ms');
+  });
+  
+  
+  
+  it("select by class is comparable to jquery (50% margin)", function() {
+    
+    var uTime = performance(function(){
+      u('.demo');
+    }, 10000);
+    
+    var $Time = performance(function(){
+      $('.demo');
+    }, 10000);
+    
+    console.log('u: ' + uTime + 'ms ', '$: ' + $Time + 'ms');
+    
+    expect(uTime).to.be.below($Time * 1.5, uTime + ' ms');
+  });
+  
+  
+  
+  it("vs jquery: class selector", function() {
+    
+    var uTime = performance(function(){
+      u('.tabletest');
+    }, 500);
+    
+    var $Time = performance(function(){
+      $('.tabletest');
+    }, 500);
+    
+    console.log('u: ' + uTime + 'ms ', '$: ' + $Time + 'ms');
+    
+    expect(uTime).to.be.below($Time, uTime + ' ms');
+  });
+  
+  
+  
+  it("vs jquery: complex selector", function() {
+    
+    var uTime = performance(function(){
+      u('table td:first-child');
+    }, 100);
+    
+    var $Time = performance(function(){
+      $('table td:first-child');
+    }, 100);
+    
+    console.log('u: ' + uTime + 'ms ', '$: ' + $Time + 'ms');
+    
+    expect(uTime).to.be.below($Time, uTime + ' ms');
+  });
+  
+  
+  
+  it("vs jquery: add class", function() {
+    
+    var uBase = u('.tabletest');
+    var $Base = $('.tabletest');
+    
+    var uTime = performance(function(){
+      uBase.addClass('test');
+    }, 100);
+    
+    var $Time = performance(function(){
+      $Base.addClass('test');
+    }, 100);
+    
+    console.log('u: ' + uTime + 'ms ', '$: ' + $Time + 'ms');
+    
+    expect(uTime).to.be.below($Time, uTime + ' ms');
+  });
+  
+  
+  
+  it("vs jquery: jquery optimized vs raw umbrella", function() {
+    
+    var uTime = performance(function(){
+      u(".ro > *");
+    }, 100);
+    
+    var $Time = performance(function(){
+      $(".ro").children();
+    }, 100);
+    
+    console.log('u: ' + uTime + 'ms ', '$: ' + $Time + 'ms');
+    
+    expect(uTime).to.be.below($Time, uTime + ' ms');
+  });
+  
+  
+  it("jquery vs jquery: mistake?", function() {
+    
+    var aTime = performance(function(){
+      $(".ro > *");
+    }, 100);
+      
+    var bTime = performance(function(){
+      $(".ro").children();
+    }, 100);
+    
+    console.log('a: ' + aTime + 'ms ', 'b: ' + bTime + 'ms');
+    
+    expect(aTime).to.be.below(bTime, uTime + ' ms');
+  });
   
   
 });
