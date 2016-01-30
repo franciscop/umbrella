d7fd6bfe617bd869a0f7f5173155fb6f97c08f05
AHB <Adam.Hampshire-Boswell@sky.uk>
2016-01-30 19:52:57 +0000

    Remove ES6 arrow functions in test file

--- test/test.js
+++ test/test.js
@@ -729,38 +729,38 @@ describe(".not(elems)", function() {
   });
 
 });
+'use strict';
+
 describe('.off()', function() {
 
-  const listener = () => {
+  const listener = function() {
     throw 'Shouldn\'t be called';
   };
 
   beforeEach(function() {
-    base.append('\
-      <ul> \
-        <li class="off-single-test">1</li> \
-        <li class="off-multiple-test">2</li> \
-        <li class="off-multiple-test">3</li> \
-      </ul>');
+    base.append('<ul><li class="off-single-test">1</li>'
+    + '<li class="off-multiple-test">2</li>'
+    + '<li class="off-multiple-test">3</li>'
+    + '</ul>');
   });
 
-  it('should be defined', () => {
-    expect(typeof base.off).to.equal('function');
+  it('should be defined', function() {
+    expect(typeof base.not).to.equal('function');
   });
 
-  it('removes event from single element', () => {
+  it('removes event from single element', function() {
     u('.off-single-test').on('click', listener);
     u('.off-single-test').off('click', listener);
     u('.off-single-test').trigger('click');
   });
 
-  it('removes event from multiple elements', () => {
+  it('removes event from multiple elements', function() {
     u('.off-multiple-test').on('click', listener);
     u('.off-multiple-test').off('click', listener);
     u('.off-multiple-test').trigger('click');
   });
 
-  it('removes multiple events', () => {
+  it('removes multiple events', function() {
     u('.off-multiple-test').on('click mouseover', listener);
     u('.off-multiple-test').off('click mouseover', listener);
     u('.off-multiple-test').trigger('mouseover');
