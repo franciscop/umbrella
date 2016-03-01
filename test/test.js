c9e47a535d5bfcde01df1f25bc211c0136da8ff9
FranciscoP <franciscopresencia@hotmail.com>
2016-03-01 19:28:17 +0900

    Fixed serialize(). Fixes #35. Fixes #41.

--- test/test.js
+++ test/test.js
@@ -1685,6 +1685,44 @@ describe(".select(selector)", function() {
 
 
 // Testing the main file
+describe(".serialize()", function() {
+
+  afterEach(function() {
+    u('.serialize-test').remove();
+  });
+
+
+  it("should be defined", function() {
+    expect(typeof u('.serialize-test').serialize).to.equal('function');
+  });
+
+  it("can handle arrays", function() {
+    base.append('\
+      <form class="serialize-test"> \
+        <input name="test[]" value="a"> \
+        <input name="test[]" value="b"> \
+      </form> \
+    ');
+    expect(u('.serialize-test').serialize()).to.equal('test%5B%5D=a&test%5B%5D=b');
+  });
+
+
+
+  it("can handle select multiple", function() {
+    base.append('\
+      <form class="serialize-test"> \
+        <select multiple name="select"> \
+          <option selected value="a"></option> \
+          <option selected value="b"></option> \
+          <option value="c"></option> \
+        </select> \
+      </form> \
+    ');
+    expect(u('.serialize-test').serialize()).to.equal('select=a&select=b');
+  });
+});
+
+// Testing the main file
 describe(".siblings(selector)", function() {
   
   beforeEach(function() {
