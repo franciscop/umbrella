41656541d19c6fe7113da7ab85d2f952f4fe673e
Ian White <ianwhite@utexas.edu>
2016-04-27 01:05:41 -0500

    rename function

--- src/plugins/wrap/wrap.js
+++ src/plugins/wrap/wrap.js
@@ -2,8 +2,8 @@ u.prototype.wrap = function(selector) {
   // 1) Construct dom node e.g. u('<a>'),
   // 2) clone the currently matched node
   // 3) append cloned dom node to constructed node based on selector
-  return this.join(function wtf(node, i) {
-    var newNode = 
+  return this.join(function outputWrap(node, i) {
+    var newNode =
       u(selector)
         .append(node.cloneNode(true))
         .each(function(newNode) {
