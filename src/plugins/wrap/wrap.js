9cbfde3acd8a736a1388cc9c4ab8cfcb1309c6a7
Ian White <ian.white@t-3.com>
2016-04-26 23:14:44 -0500

    tweaked wrap.js

--- src/plugins/wrap/wrap.js
+++ src/plugins/wrap/wrap.js
@@ -2,7 +2,7 @@ u.prototype.wrap = function(selector) {
   // 1) Construct dom node e.g. u('<a>'),
   // 2) clone the currently matched node
   // 3) append cloned dom node to constructed node based on selector
-  this.nodes = this.join(function wtf(node, i) {
+  return this.join(function wtf(node, i) {
     var newNode = 
       u(selector)
         .append(node.cloneNode(true))
@@ -11,12 +11,11 @@ u.prototype.wrap = function(selector) {
             parentNode.
             replaceChild(newNode, node);
         });
-        
-    return newNode.nodes;
-  });
 
   // Update new nodes list to be passed
   // along to any possible chained functions
   // e.g. .attr, .addClass, etc
-  return this.nodes;
+  // return this.nodes;
+    return newNode.nodes;
+  });
 };
