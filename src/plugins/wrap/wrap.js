eeb8210e6967b5e80a35a5d275cb3d2babcac390
Ian White <ian.white@t-3.com>
2016-04-27 09:42:29 -0500

    streamline wrap.js

--- src/plugins/wrap/wrap.js
+++ src/plugins/wrap/wrap.js
@@ -3,19 +3,16 @@ u.prototype.wrap = function(selector) {
   // 2) clone the currently matched node
   // 3) append cloned dom node to constructed node based on selector
   return this.join(function outputWrap(node, i) {
-    var newNode =
-      u(selector)
-        .append(node.cloneNode(true))
-        .each(function(newNode) {
-          node.
-            parentNode.
-            replaceChild(newNode, node);
-        });
-
   // Update new nodes list to be passed
   // along to any possible chained functions
   // e.g. .attr, .addClass, etc
   // return this.nodes;
-    return newNode.nodes;
+    return  u(selector)
+              .append(node.cloneNode(true))
+              .each(function(newNode) {
+                node.
+                  parentNode.
+                  replaceChild(newNode, node);
+              }).nodes;
   });
 };
--- umbrella.js
+++ umbrella.js
@@ -815,20 +815,17 @@ u.prototype.wrap = function(selector) {
   // 1) Construct dom node e.g. u('<a>'),
   // 2) clone the currently matched node
   // 3) append cloned dom node to constructed node based on selector
-  return this.join(function wtf(node, i) {
-    var newNode = 
-      u(selector)
-        .append(node.cloneNode(true))
-        .each(function(newNode) {
-          node.
-            parentNode.
-            replaceChild(newNode, node);
-        });
-
+  return this.join(function outputWrap(node, i) {
   // Update new nodes list to be passed
   // along to any possible chained functions
   // e.g. .attr, .addClass, etc
   // return this.nodes;
-    return newNode.nodes;
+    return  u(selector)
+              .append(node.cloneNode(true))
+              .each(function(newNode) {
+                node.
+                  parentNode.
+                  replaceChild(newNode, node);
+              }).nodes;
   });
 };
