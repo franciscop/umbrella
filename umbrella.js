4ff4449ba0aa317ded3bccb05ff7ef8ec29ecefa
amugisha <alain-pax.mugisha@amadeus.com>
2016-05-01 18:11:47 +0100

    resolve merge conflicts Revert "adds updated global files"
    This reverts commit fdf016f88bd54079ae826073f98af503276a6804.

--- umbrella.js
+++ umbrella.js
@@ -583,33 +583,6 @@ u.prototype.removeClass = function() {
 
 
 /**
- * .replace()
- *
- * Replace the matched elements with the passed argument.
- * @return this Umbrella object
- */
-u.prototype.replace = function(newValue) {
-    var newNode;
-    var paramType = typeof newValue;
-    if (paramType === 'string') {
-       return this.join(function(node){
-           newNode = u(newValue).first();
-           node.parentNode.replaceChild(newNode, node);
-           return newNode;
-        });
-    } else if (paramType === 'function'){
-        return this.join(function(node) {
-            newNode = u(newValue(node)).first();
-            node.parentNode.replaceChild(newNode, node);
-            return newNode;
-        });
-    } else {
-        return this;
-    }
-};
-
-
-/**
  * .scroll()
  *
  * Scroll to the first matched element
