7971a2d17714091875c295c2dd77423730efa275
Ian White <ian.white@t-3.com>
2016-05-28 01:10:13 -0500

    call out jQuery#clone

--- src/plugins/clone/clone.js
+++ src/plugins/clone/clone.js
@@ -1,17 +1,18 @@
 u.prototype.clone = function clone () {
   return this.map(function (node, i) {
-    var cloneWithEvents = node.cloneNode(true);
+    var clone = node.cloneNode(true);
     var l;
     var srcElements = getAll(node);
-    var destElements = getAll(cloneWithEvents);
+    var destElements = getAll(clone);
 
     for (i = 0, l = srcElements.length; i < l; i++) {
       cloneCopyEvent(srcElements[ i ], destElements[ i ]);
     }
 
-    return cloneWithEvents;
+    return clone;
   });
 
+  // Mostly code borrowed from jQuery: https://github.com/jquery/jquery/blob/305f193aa57014dc7d8fa0739a3fefd47166cd44/src/manipulation.js
   // These should probably be moved elsewhere
   // TODO: form input support. data attr support?
   function getAll (context, tag) {
