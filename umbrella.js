d72ce6ed04ab01b6dccc6f6945c3c1085bb320c3
Ian White <ian.white@t-3.com>
2016-05-29 13:29:45 -0500

    add internal mirror object to allow clone method to be extensible. see https://github.com/umbrellajs/umbrella/issues/64

--- umbrella.js
+++ umbrella.js
@@ -195,43 +195,49 @@ u.prototype.children = function (selector) {
 };
 
 
+u.prototype.mirror = {};
+
+u.prototype.mirror.events = function events (source, destination) {
+  var i;
+  var l;
+  var type;
+  var events;
+
+  if (source._e) {
+    events = source._e;
+    for (type in events) {
+      for (i = 0, l = events[type].length; i < l; i++) {
+        u(destination).on(type, events[type][i]);
+      }
+    }
+  }
+};
+
+u.prototype.getAll = function getAll (context, tag) {
+  // Mostly code borrowed from jQuery: https://github.com/jquery/jquery/blob/305f193aa57014dc7d8fa0739a3fefd47166cd44/src/manipulation.js
+  var rtn = u(tag || '*', context);
+  return rtn.length ? rtn.nodes : [context];
+};
+
 u.prototype.clone = function clone () {
   return this.map(function (node, i) {
     var clone = node.cloneNode(true);
     var l;
-    var srcElements = getAll(node);
-    var destElements = getAll(clone);
+    var srcElements = this.getAll(node);
+    var destElements = this.getAll(clone);
 
     for (i = 0, l = srcElements.length; i < l; i++) {
-      cloneCopyEvent(srcElements[ i ], destElements[ i ]);
+      this.mirror.events(srcElements[ i ], destElements[ i ]);
+    }
+
+    for (var key in this.mirror) {
+      if (this.mirror[key].name !== 'events') {
+        this.mirror[key](srcElements[ i ], destElements[ i ]);
+      }
     }
 
     return clone;
   });
-
-  // Mostly code borrowed from jQuery: https://github.com/jquery/jquery/blob/305f193aa57014dc7d8fa0739a3fefd47166cd44/src/manipulation.js
-  // These should probably be moved elsewhere
-  // TODO: form input support. data attr support?
-  function getAll (context, tag) {
-    var rtn = u(tag || '*', context);
-    return rtn.length ? rtn.nodes : [context];
-  }
-
-  function cloneCopyEvent (source, destination) {
-    var i;
-    var l;
-    var type;
-    var events;
-
-    if (source._e) {
-      events = source._e;
-      for (type in events) {
-        for (i = 0, l = events[type].length; i < l; i++) {
-          u(destination).on(type, events[type][i]);
-        }
-      }
-    }
-  }
 };
 
 
