4299aaf6a23ad231610e2a115255e2612d165471
Bartosz Lorek <bery.lorek@gmail.com>
2017-05-08 16:12:42 +0200

    css.js with tests and docs

--- umbrella.js
+++ umbrella.js
@@ -283,6 +283,38 @@ u.prototype.closest = function (selector) {
 };
 
 
+// Set or retrieve the computed style from the matched node(s)
+u.prototype.css = function (prop, value) {
+  // multiple styles wrapped in object
+  if (typeof prop === 'object') {
+    for (var style in prop) {
+      if (prop.hasOwnProperty(style)) {
+        this.css(style, prop[style]);
+      }
+    }
+    return this;
+  }
+
+  // convert kebab-case to camelCase
+  prop = prop.replace(/-([a-z])/g, function (a) {
+    return a[1].toUpperCase();
+  });
+
+  if (value === undefined) {
+    return this.first().style[prop];
+  }
+
+  // number without unit
+  if (typeof value === 'number') {
+    value += 'px';
+  }
+
+  return this.each(function (node) {
+    node.style[prop] = value;
+  });
+};
+
+
 // Handle data-* attributes for the matched elements
 u.prototype.data = function (name, value) {
   return this.attr(name, value, true);
@@ -321,7 +353,6 @@ u.prototype.empty = function () {
     }
   });
 };
-
 
 
 // .filter(selector)
