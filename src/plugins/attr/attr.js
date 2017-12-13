4e9a9420725e478acb6f66c71e6d7da3a57f99ad
Francisco Presencia <public@francisco.io>
2017-12-13 14:38:48 +0900

    Small fix of remaining code

--- src/plugins/attr/attr.js
+++ src/plugins/attr/attr.js
@@ -5,31 +5,10 @@ u.prototype.attr = function (name, value, data) {
   data = data ? 'data-' : '';
 
   // This will handle those elements that can accept a pair with these footprints:
-  // .css('a'), .css('a', 'b'), .css({ a: 'b' })
+  // .attr('a'), .attr('a', 'b'), .attr({ a: 'b' })
   return this.pairs(name, value, function (node, name) {
     return node.getAttribute(data + name);
   }, function (node, name, value) {
     node.setAttribute(data + name, value);
   });
-};
-
-// Handle attributes for the matched elements
-u.prototype.attr2 = function (name, value, data) {
-  data = data ? 'data-' : '';
-
-  if (value !== undefined) {
-    var nm = name;
-    name = {};
-    name[nm] = value;
-  }
-
-  if (typeof name === 'object') {
-    return this.each(function (node) {
-      for (var key in name) {
-        node.setAttribute(data + key, name[key]);
-      }
-    });
-  }
-
-  return this.length ? this.first().getAttribute(data + name) : '';
 };
