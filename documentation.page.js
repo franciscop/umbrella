39a454c18971af74a8a56a55f2a7e10430dcdbdd
Francisco Presencia <franciscop@users.noreply.github.com>
2022-07-01 11:00:05 +0900

    Create documentation.page.js

---
+++ documentation.page.js
@@ -0,0 +1,9 @@
+{
+  "title": "☔ Lightweight javascript library for DOM manipulation and events",
+  "menu": {
+    "Documentation": "https://docs.umbrellajs.com/",
+    "Get help": "https://superpeer.com/francisco/-/javascript-and-react-help",
+    "Github": "https://github.com/franciscop/umbrella"
+  },
+  "pages": ["documentation.md"]
+}
